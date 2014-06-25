0d350d2174b0d579772c840be4bed457a145b52d
Brett Zamir <brettz9@yahoo.com>
2014-06-25 19:27:12 +0800

    Apply fix courtesy nmaier from http://stackoverflow.com/a/24406145/271577

--- data/findAncestor.js
+++ data/findAncestor.js
@@ -6,37 +6,17 @@ function l (s) {console.log(s);}
 
 var x = 0, y = 0;
 window.addEventListener('click', function (e) {
-	// if (e.button === 2) { // Avoid grabbing for the actual selection // Doesn't seem to execute on single click anyways
-		x = e.pageX;
-		y = e.pageY;
-	// }
+	if (e.button === 2) { // Avoid grabbing for the actual selection // Doesn't seem to execute on single click anyways but add for good measure
+		x = e.clientX;
+		y = e.clientY;
+	}
 });
 self.on('click', function () { // , data
-	// For some reason, this does not seem to work very well
-	x = x > window.mozInnerScreenX ? window.mozInnerScreenX : (x < 0 ? 0 : x);
-	y = y > window.mozInnerScreenY ? window.mozInnerScreenY : (y < 0 ? 0 : y);
+	x = Math.max(0, Math.min(window.innerWidth, x));
+	y = Math.max(0, Math.min(window.innerHeight, y));
 	var caretPosition = document.caretPositionFromPoint(x, y);
 	var node = caretPosition.offsetNode;
 
-	/*
-	// Only declare functions when context menu clicked
-	// Adapted from https://developer.mozilla.org/en-US/docs/Web/Guide/API/DOM/Whitespace_in_the_DOM
-	var is_all_ws = function (nod) {
-		return !(/[^\t\n\r ]/.test(nod.textContent));
-	};
-	var is_ignorable = function (nod) {
-		return (nod.nodeType === 8) || // A comment node
-			((nod.nodeType === 3) && is_all_ws(nod)); // a text node, all ws
-	};
-	function first_significant_child (par) {
-		var res = par.firstChild;
-		while (res) {
-			if (!is_ignorable(res)) {return res;}
-			res = res.nextSibling;
-		}
-		return null;
-	}
-	*/
 	var findDeepestLastChild = function (elem) {
 		var oldElem;
 		do {
@@ -58,20 +38,7 @@ self.on('click', function () { // , data
 			if (foundAnchor(node)) {
 				break;
 			}
-			/*
-			var fsc, fec, deepestSafeNode = node;
-			while (deepestSafeNode) { // e.g., the first node returned was <h1><a name="pg1"></a> Text</h1>
-				fsc = first_significant_child(deepestSafeNode);
-				fec = deepestSafeNode.firstElementChild;
-				if (!fec || fsc !== fec) {
-					break;
-				}
-				if (foundAnchor(fec)) {
-					throw 'escape';
-				}
-				deepestSafeNode = fec;
-			}
-			*/
+
 			if (node.previousElementSibling) {
 				node = findDeepestLastChild(node.previousElementSibling);
 			}
