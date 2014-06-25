57702c880f4537d78ff2baf85c02951221ce0437
Brett Zamir <brettz9@yahoo.com>
2014-06-25 20:51:02 +0800

    polling mouseover attempt (still imperfect)

--- data/findAncestor.js
+++ data/findAncestor.js
@@ -1,21 +1,37 @@
 /*globals self */
 /*jslint vars:true, browser:true*/
-(function (undefined) {'use strict';
+(function () {'use strict';
 
 function l (s) {console.log(s);}
 
-var x, y;
+var x = 0, y = 0, coordCatching;
+
+function coordCatcher (e) {l('a');
+    x = e.clientX;
+    y = e.clientY;
+    l('x:'+x);
+    // Set up a less accurate but less performance-intensive polling of mouse coords
+    window.removeEventListener('mouseover', coordCatcher, true);
+    coordCatching = setTimeout(function () {
+        l('timeout');
+        window.addEventListener('mouseover', coordCatcher, true);
+    }, 100);
+}
+// Listen via mousemove until click events start registering
+window.addEventListener('mouseover', coordCatcher, true);
+
 window.addEventListener('click', function (e) {
-	//if (e.button === 2) { // Avoid grabbing for the actual selection // Doesn't seem to execute on single click anyways but add for good measure
-		x = e.clientX;
-		y = e.clientY;
-	//}
-});
-self.on('click', function () { // , data
-    if (x === undefined) {
-        document.documentElement.click(); // Ensure click event occurs as it does not always do so on page load for some reason
-        l(x); // On first attempt is always 0 for some reason!
+    if (coordCatching) {
+        window.removeEventListener('mouseover', coordCatcher, true);
+        clearTimeout(coordCatching);
     }
+    if (e.button === 2) { // Avoid grabbing for the actual selection // Doesn't seem to execute on single click anyways but add for good measure
+        x = e.clientX;
+        y = e.clientY;
+    }
+}, true);
+
+self.on('click', function () { // , data
 	x = Math.max(0, Math.min(window.innerWidth, x));
 	y = Math.max(0, Math.min(window.innerHeight, y));
 	var caretPosition = document.caretPositionFromPoint(x, y);
