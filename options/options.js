4de0c4c2e0dbff87d8110f524a1819e329e87e51
Brett Zamir <brettz9@yahoo.com>
2023-04-04 13:47:48 -0700

    fix options for Manifest v3

--- options/options.js
+++ options/options.js
@@ -14,30 +14,34 @@ function _ (...args) {
 
 document.title = _('jumpToAnchor'); // If switch to tabs
 
-// eslint-disable-next-line unicorn/prefer-top-level-await -- Envt
-(async () => {
-const backgroundPage = await browser.extension.getBackgroundPage();
-const {isFirefox} = backgroundPage;
-const {
-    separateContextMenus = await isFirefox()
-} = await browser.storage.local.get(
-    'separateContextMenus'
-);
-jml('section', [
-    ['label', [
-        _('separateContextMenus') + ' ',
-        ['input', {
-            type: 'checkbox',
-            checked: separateContextMenus,
-            $on: {
-                async change () {
-                    await browser.storage.local.set({
-                        separateContextMenus: this.checked
-                    });
-                    backgroundPage.updateContextMenus();
+const port = browser.runtime.connect({name: 'options-port'});
+
+port.onMessage.addListener(async ({message, isFirefox}) => {
+    if (message !== 'isFirefox') {
+        return;
+    }
+    const {
+        separateContextMenus = isFirefox
+    } = await browser.storage.local.get(
+        'separateContextMenus'
+    );
+    jml('section', [
+        ['label', [
+            _('separateContextMenus') + ' ',
+            ['input', {
+                type: 'checkbox',
+                checked: separateContextMenus,
+                $on: {
+                    async change () {
+                        await browser.storage.local.set({
+                            separateContextMenus: this.checked
+                        });
+                        // eslint-disable-next-line max-len -- Long
+                        // eslint-disable-next-line unicorn/require-post-message-target-origin -- WebExt
+                        port.postMessage({message: 'updateContextMenus'});
+                    }
                 }
-            }
-        }]
-    ]]
-], body);
-})();
+            }]
+        ]]
+    ], body);
+});
